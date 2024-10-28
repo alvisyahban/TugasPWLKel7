3d14a2bf806fc310ec6e339171b6b34d63161f74
M Alvi Syahban <alvisyahban217@gmail.com>
2024-10-28 11:36:15 +0700

    Register dan Login

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors :{
+        primary : {},
+      }
+    },
+  },
+  plugins: [],
+}
+
