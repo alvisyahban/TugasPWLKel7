c9b8a74c117f8d61be687c130a091227adbe1808
M Alvi Syahban <alvisyahban217@gmail.com>
2024-10-29 11:47:37 +0700

    menambahkan assets dan mengubah config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,56 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors :{
-        primary : {},
-      }
+    colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        blue: colors.blue,
+        cyan: colors.cyan,
+        emerald: colors.emerald,
+        fuchsia: colors.fuchsia,
+        slate: colors.slate,
+        gray: colors.gray,
+        neutral: colors.neutral,
+        stone: colors.stone,
+        green: colors.green,
+        indigo: colors.indigo,
+        lime: colors.lime,
+        orange: colors.orange,
+        pink: colors.pink,
+        purple: colors.purple,
+        red: colors.red,
+        rose: colors.rose,
+        sky: colors.sky,
+        teal: colors.teal,
+        violet: colors.violet,
+        yellow: colors.amber,
+        white: colors.white,
     },
+    fontSize: {
+        'xs': '0.75rem',
+        'sm': '0.8rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+        '4xl': '2.4rem',
+        '5xl': '3rem',
+    },
+    extend: {
+      fontFamily: {
+          sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
+      inset: {
+          '3px': '3px',
+      },
+      borderRadius: {
+          'md': '12px',
+          'xl': '2rem',
+          '3xl': '50px'
+      },
+  },
   },
   plugins: [],
 }
